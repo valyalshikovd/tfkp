8b678a3df155b5ffa3a8d796849a605211d33357
valyalshikovd <770vda@mail.ru>
2024-01-19 02:39:34 +0300

    v1.1test

--- src/Question.js
+++ src/Question.js
@@ -5,6 +5,7 @@ const Question = (props) => {
     const imageUrl = process.env.PUBLIC_URL + '/questions/q';
     return (
         <div>
+            process.env.PUBLIC_URL
             <img src={imageUrl + photoPath + ".png"} alt={imageUrl + photoPath + ".png"}  />
         </div>
     );
