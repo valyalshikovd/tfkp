f454b86f8e77629fba3c19ea783dbb58be7d5acd
valyalshikovd <770vda@mail.ru>
2024-01-19 03:10:14 +0300

    v1.82test

--- src/Question.js
+++ src/Question.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Question = (props) => {
     const { photoPath, altText } = props;
-    const imageUrl = 'public/static' + '/questions/q';
+    const imageUrl = process.env.PUBLIC_URL + '/questions/q';
     return (
         <div>
             <div>
                 {process.env.PUBLIC_URL}
             </div>
-            <img src={imageUrl + photoPath + ".png"} alt={imageUrl + photoPath + ".PNG"}  />
+            <img src={imageUrl + photoPath + ".PNG"} alt={imageUrl + photoPath + ".PNG"}  />
         </div>
     );
 };
